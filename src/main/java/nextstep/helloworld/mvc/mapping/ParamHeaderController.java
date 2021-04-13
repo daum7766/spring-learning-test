c19e9e10a03288b4463871411309b0cf3f4d59d8
daum7766 <daum7766@naver.com>
2021-04-13 18:23:33 +0900

    feat: Param Header

--- src/main/java/nextstep/helloworld/mvc/mapping/ParamHeaderController.java
+++ src/main/java/nextstep/helloworld/mvc/mapping/ParamHeaderController.java
@@ -1,23 +1,29 @@
 package nextstep.helloworld.mvc.mapping;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/param-header")
 public class ParamHeaderController {
 
+    @GetMapping(path = "/message")
     public ResponseEntity<String> message() {
         return ResponseEntity.ok().body("message");
     }
 
-    public ResponseEntity<String> messageForParam() {
-        return ResponseEntity.ok().body("hello");
+    @GetMapping(path = "/message", params = "name=hello")
+    public ResponseEntity<String> messageForParam(@RequestParam String name) {
+        return ResponseEntity.ok().body(name);
     }
 
-    public ResponseEntity<String> messageForHeader() {
-        return ResponseEntity.ok().body("hi");
+    @GetMapping(path = "/message", headers = "HEADER=hi")
+    public ResponseEntity<String> messageForHeader(@RequestHeader HttpHeaders header) {
+        return ResponseEntity.ok().body(header.getFirst("HEADER"));
     }
 }
